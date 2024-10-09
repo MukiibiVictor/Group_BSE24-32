ad43830d78ae545556601d9a2384b489e76d8b91
Bindya Philip <philbindya55@gmail.com>
2024-10-09 14:21:08 +0300

    made changes required for the staging environment, hope it works

--- Cake Addicts/cakeaddicts/settings.py
+++ Cake Addicts/cakeaddicts/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cakeaddicts.urls'
@@ -78,10 +81,9 @@ WSGI_APPLICATION = 'cakeaddicts.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+    
 }
 
 
